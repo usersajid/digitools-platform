9b00b8ac395d800631e260fa1a053e205eaf578c
Sajid Hasan <sajidhasan164@gmail.com>
2026-03-31 11:40:29 +0600

    React Vite Tailwind and live link setup completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+    <h1 className='text-3xl font-bold underline text-center mt-10'>Hello world!</h1>
+    </>
+  )
+}
+
+export default App
